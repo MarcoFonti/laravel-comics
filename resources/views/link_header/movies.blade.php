82fa62b7496f8f8a2c608bd3512cd8ab8f521d6e
MarcoFonti <theemporiumg@gmail.com>
2024-03-09 15:12:03 +0100

    add single movie

--- resources/views/link_header/movies.blade.php
+++ resources/views/link_header/movies.blade.php
@@ -1,74 +1,23 @@
+{{-- RECUPERO LAYOUT BASE --}}
 @extends('layout.app')
 
+{{-- CREO TITOLO --}}
 @section('title', 'Movies')
-    
 
+{{-- CREO CONTENUTO --}}
 @section('main-content')
-<!-- MAIN MOVIES -->
-    <section id="recipe-jumbotro">
-        <div class="recipe-movies"></div>
-        <img class="image-movies" src="{{ asset('images/adv.jpg') }}" alt="">
-    </section>
-    <!-- SEZIONE CONTENUTO MENU -->
-    <section id="recipe-content-movies">
-        <article class="article-movies">
-            <h1 class="title-movies">fffffkofkoko</h1>
-            <div class="movies-info">
-                <div class="movies-price">
-                    <div>U.S Price: $19.99</div>
-                    <div>AVAILABLE</div>
-                </div>
-                <div>Check Availability</div>
+    <section id="recipe-content">
+        <!-- CONTENITORE FILM -->
+        <div class="container-films">
+            @foreach ( $comics as $index => $film )
+            <div class="recipe-films">
+                <a href="{{ route('movie', $index) }}">
+                    <img src="{{ $film['thumb'] }}" alt="">
+                    <div class="text-films"></div>
+                </a>
             </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis rerum voluptates iusto reprehenderit error sapiente inventore nam, possimus explicabo vero quia vel eos temporibus impedit veniam eligendi. Possimus, voluptas. Perferendis!
-                Nemo quia delectus assumenda aliquam ab deserunt, a temporibus deleniti eveniet tempore fugiat ullam molestiae, impedit officiis nulla magni id soluta, eos dolore inventore possimus sapiente tenetur? Culpa, deserunt sed.
-                Porro, fuga sed, aspernatur nobis magnam, in eaque sequi quam adipisci eveniet numquam facere laborum nisi quibusdam nemo voluptas aperiam minus earum amet ipsam repellat voluptatum! Voluptatem debitis beatae laboriosam!</p>
-            </div>
-        </article>
-        <figure>
-            <h5 class="text-movies">ADVERTISEMENT</h5>
-            <img class="path-movies" src="{{ asset('images/adv.jpg') }}" alt="">
-        </figure>
-    </section>
-    <!-- SEZIONE INFORMAZIONI FILM -->
-    <section id="recipe-info-movies">
-        <div class="container-movies">
-            <div class="recipe-talent">
-                <h3 class="title-info">Talent</h3>
-                <div class="d-flex-movies">
-                    <h6 class="title-action">Art by:</h6>
-                    <div class="actors-movies text-blue">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius beatae incidunt vero magni veritatis culpa a porro.</div>
-                </div>
-                <div class="d-flex-movies">
-                    <h6 class="title-action">Written by:</h6>
-                    <div class="actors-movies text-blue">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius beatae incidunt vero magni veritatis culpa a porro.</div>
-                </div>
-            </div>
-            <div class="recipe-specs">
-                <h3 class="title-info">Specs</h3>
-                <div class="d-flex-movies">
-                    <h6 class="title-action">Series:</h6>
-                    <div class="title-margin text-blue">ACTION COMICS</div>
-                </div>
-                <div class="d-flex-movies">
-                    <h6 class="title-action">U.S Price:</h6>
-                    <div class="title-margin">$19.99</div>
-                </div>
-                <div class="d-flex-movies">
-                    <h6 class="title-action">On Sale Date:</h6>
-                    <div class="title-margin">Oct 02 2018</div>
-                </div>
-            </div>
+            @endforeach
         </div>
-    </section>
-    <!-- SEZIONE CONTENUTO MENU -->
-    <section id="recipe-content-link-movies">
-        @foreach ( $main_menu as $menu )  
-            <div class="recipe-comics">
-                <img class="image-link-movies" src="{{ asset('images/'. $menu['image']) }}" alt="{{ $menu['text'] }}">
-                <span>{{ $menu['text'] }}</span>
-            </div>
-        @endforeach
+        <button class="button-films">load more</button>
     </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,62 +17,112 @@ use Illuminate\Support\Facades\Route;
 /* HOME */
 Route::get('/', function () {
 
+    /* RECUPERO VALORI DEL FILE COMICS */
     $comics = config('comics');
 
+    /* RECUPERO VALORI DEL FILE MAIN_MENU */
     $main_menu = config('main_menu');
 
     return view('home', compact('comics', 'main_menu'));
+
 })-> name('home');
+
 
 /* CHARACTERS */
 Route::get('/characters', function () {
+
     return view('link_header.characters');
+
 })-> name('characters');
+
 
 /* COMICS */
 Route::get('/comics', function () {
+
     return view('link_header.comics');
+
 })-> name('comics');
+
 
 /* MOVIES */
 Route::get('/movies', function () {
+    
+    /* RECUPERO VALORI DEL FILE COMICS */
+    $comics = config('comics');
 
-    $main_menu = config('main_menu');
+    return view('link_header.movies', compact('comics'));
 
-    return view('link_header.movies', compact('main_menu'));
 })-> name('movies');
+
+
+/* SINGOLO MOVIE */
+Route::get('/movies/{index}', function ($index) { 
+
+    /* RECUPERO VALORI DEL FILE MAIN_MENU */
+    $main_menu = config('main_menu');
+    
+    /* RECUPERO VALORI DEL FILE COMICS */
+    $movies = config('comics');
+
+    /* RECUPERO INDICE DI OGNI COMICS */
+    $movie = $movies[$index];
+    
+    return view('movies.movie', compact('main_menu','movie') );
+
+})-> name('movie');
+
 
 /* TV */
 Route::get('/tv', function () {
+
     return view('link_header.tv');
+
 })-> name('tv');
+
 
 /* GAMES */
 Route::get('/games', function () {
+
     return view('link_header.games');
+
 })-> name('games');
+
 
 /* COLLECTIBLES */
 Route::get('/collectibles', function () {
+
     return view('link_header.collectibles');
+
 })-> name('collectibles');
+
 
 /* VIDEOS */
 Route::get('/videos', function () {
+
     return view('link_header.videos');
+
 })-> name('videos');
+
 
 /* FANS */
 Route::get('/fans', function () {
+
     return view('link_header.fans');
+
 })-> name('fans');
+
 
 /* NEWS */
 Route::get('/news', function () {
+
     return view('link_header.news');
+
 })-> name('news');
+
 
 /* SHOP */
 Route::get('/shop', function () {
+
     return view('link_header.shop');
+
 })-> name('shop');
